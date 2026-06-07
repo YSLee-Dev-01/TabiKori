ba6779f501074d4ee70d76edbcb822f075c829cc
YSLee-Dev-01 <ysleedev01@gmail.com>
2026-06-07 12:50:03 +0900

    Initial Commit

---
+++ TabiKori/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TabiKori
+//
+//  Created by 이윤수 on 6/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TabiKori/TabiKoriApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabiKoriApp.swift
+//  TabiKori
+//
+//  Created by 이윤수 on 6/7/26.
+//
+
+import SwiftUI
+
+@main
+struct TabiKoriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
