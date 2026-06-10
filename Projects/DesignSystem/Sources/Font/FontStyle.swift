4a8c82837548aaebaf8ba7e189d02e0bfaf28402
YSLee-Dev-01 <ysleedev01@gmail.com>
2026-06-10 21:14:46 +0900

    Design: 프로젝트에서 사용할 폰트 추가 및 FontStyle 추가

---
+++ Projects/DesignSystem/Sources/Font/FontStyle.swift
@@ -0,0 +1,41 @@
+//
+//  FontStyle.swift
+//  DesignSystem
+//
+//  Created by 이윤수 on 6/10/26.
+//  Copyright © 2026 yslee. All rights reserved.
+//
+
+import SwiftUI
+
+public enum PretendardWeight {
+    case thin
+    case extraLight
+    case light
+    case regular
+    case medium
+    case semiBold
+    case bold
+    case extraBold
+    case black
+
+    var fontWeight: Font.Weight {
+        switch self {
+        case .thin:       return .ultraLight
+        case .extraLight: return .thin
+        case .light:      return .light
+        case .regular:    return .regular
+        case .medium:     return .medium
+        case .semiBold:   return .semibold
+        case .bold:       return .bold
+        case .extraBold:  return .heavy
+        case .black:      return .black
+        }
+    }
+}
+
+public extension Font {
+    static func pretendard(_ weight: PretendardWeight = .regular, size: CGFloat) -> Font {
+        .custom("PretendardJPVariable-Regular", size: size).weight(weight.fontWeight)
+    }
+}
