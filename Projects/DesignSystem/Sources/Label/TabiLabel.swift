fa7f346feb716e3c997435820d784e9c789a1feb
YSLee-Dev-01 <ysleedev01@gmail.com>
2026-06-11 22:27:26 +0900

    Feat(DesignSystem): 커스텀 Text View 추가

--- Projects/DesignSystem/Sources/Label/TabiLabel.swift
+++ Projects/DesignSystem/Sources/Label/TabiLabel.swift
@@ -6,4 +6,34 @@
 //  Copyright © 2026 yslee. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+public struct TabiLabel: View {
+    private let title: String
+    private let style: TypographyStyle
+    private let alignment: Alignment
+    private let isExpanded: Bool
+    private let lineLimit: Int?
+    
+    public init(
+        title: String,
+        style: TypographyStyle,
+        alignment: Alignment = .leading,
+        isExpanded: Bool = false,
+        lineLimit: Int? = nil
+    ) {
+        self.title = title
+        self.style = style
+        self.alignment = alignment
+        self.isExpanded = isExpanded
+        self.lineLimit = lineLimit
+    }
+    
+    public var body: some View {
+        Text(self.title)
+            .font(.pretendard(self.style.weight, size: self.style.size))
+            .lineLimit(self.lineLimit)
+            .multilineTextAlignment(self.alignment.textAlignment)
+            .frame(maxWidth: self.isExpanded ? .infinity : nil, alignment: self.alignment)
+    }
+}
--- Projects/DesignSystem/Sources/Label/TypographyStyle.swift
+++ Projects/DesignSystem/Sources/Label/TypographyStyle.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+
+public enum TypographyStyle {
+    case titleL
+    case titleM
+    case titleS
+
+    case bodyLBold
+    case bodyL
+    case bodyMBold
+    case bodyM
+    case bodySBold
+    case bodyS
+
+    case captionM
+    case captionS
+    
+    public var size: CGFloat {
+        switch self {
+        case .titleL: return 24
+        case .titleM: return 20
+        case .titleS:  return 18
+        case .bodyLBold, .bodyL: return 18
+        case .bodyMBold, .bodyM: return 16
+        case .bodySBold, .bodyS: return 14
+        case .captionM: return 13
+        case .captionS: return 12
+        }
+    }
+
+    public var weight: PretendardWeight {
+        switch self {
+        case .titleL: return .bold
+        case .titleM, .titleS: return .semiBold
+        case .bodyLBold, .bodyMBold, .bodySBold: return .bold
+        case .bodyL, .bodyM, .bodyS: return .regular
+        case .captionM, .captionS: return .regular
+        }
+    }
+}
