dabb16d30ce4e25c5e296b4bbe6aa8b65b7fe882
YSLee-Dev-01 <ysleedev01@gmail.com>
2026-06-13 16:01:14 +0900

    Refactor(Resource): 컬러는 Resource 모듈에서 관리하도록 변경

---
+++ Projects/Resource/Sources/Color/TabiColor.swift
@@ -0,0 +1,57 @@
+//
+//  TabiColor.swift
+//  Resource
+//
+//  Created by 이윤수 on 6/13/26.
+//  Copyright © 2026 yslee. All rights reserved.
+//
+
+import SwiftUI
+
+public enum TabiColor: String {
+
+    // MARK: - Brand
+
+    case tabiPrimary
+    case tabiPrimaryLight
+    case tabiPrimaryDark
+    case tabiSecondary
+
+    // MARK: - Accent
+
+    case tabiAccentCoral
+    case tabiAccentMint
+    case tabiAccentLavender
+    case tabiAccentAmber
+
+    // MARK: - Background
+
+    case tabiBackground
+    case tabiSurface
+    case tabiSurfaceElevated
+    case tabiBorder
+
+    // MARK: - Text
+
+    case tabiTextPrimary
+    case tabiTextSecondary
+    case tabiTextTertiary
+
+    // MARK: - Semantic
+
+    case tabiDestructive
+    case tabiOnColor
+
+    // MARK: - Category
+
+    case categorySightseeing
+    case categoryFood
+    case categoryHotel
+    case categoryFestival
+    case categoryShopping
+    case categoryNature
+
+    public var color: Color {
+        Color(self.rawValue, bundle: .module)
+    }
+}
