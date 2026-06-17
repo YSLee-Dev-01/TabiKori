d69160685bab213a9afbedbb9d985880ef277736
YSLee-Dev-01 <ysleedev01@gmail.com>
2026-06-17 21:39:44 +0900

    Feat(Main > Home): TabiNavigationBar, TabiGlassIconButton 적용 (CC)

--- Projects/Presentation/Sources/Home/HomeView.swift
+++ Projects/Presentation/Sources/Home/HomeView.swift
@@ -7,18 +7,38 @@
 //
 
 import SwiftUI
+
+import DesignSystem
 import ComposableArchitecture
 
 public struct HomeView: View {
     
     @State private var store: StoreOf<HomeFeature>
     
+    private let testColor = [Color.red, Color.blue, Color.green]
+    
     public init(store: StoreOf<HomeFeature>) {
         self.store = store
     }
     
     public var body: some View {
-        Text(self.store.text)
+        VStack(spacing: 0) {
+            ScrollView {
+                ForEach((0 ... 40).map {"\($0)"}, id: \.self) { text in
+                    Text(text)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 100)
+                        .background(testColor.randomElement()!)
+                }
+            }
+            .safeAreaBar(edge: .top) {
+                TabiNavigationBar(subtitle: "6월 17일 (수)", title: "타비코리") {
+                    TabiGlassIconButton(systemName: "bell") {
+                        print("123")
+                    }
+                }
+            }
+        }
     }
 }
 
