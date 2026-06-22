bfce4d5acc08231351db31264533371930ad33b0
YSLee-Dev-01 <ysleedev01@gmail.com>
2026-06-22 21:33:52 +0900

    Feat(Main > Home): 홈 화면 날짜 타이틀용 Date Extension 추가 (CC)

---
+++ Projects/Presentation/Sources/Extension/Date+.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+extension Date {
+    var homeDateTitle: String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ja_JP")
+        formatter.dateFormat = "M月d日(E)"
+        return formatter.string(from: self)
+    }
+}
