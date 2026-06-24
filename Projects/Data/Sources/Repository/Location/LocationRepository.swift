79da158c9734643e3fdefebf984e4fd0a041593e
YSLee-Dev-01 <ysleedev01@gmail.com>
2026-06-24 20:31:27 +0900

    Feat(Data): Location을 관리하는 Repository 추가

---
+++ Projects/Data/Sources/Repository/Location/LocationRepository.swift
@@ -0,0 +1,53 @@
+//
+//  LocationRepository.swift
+//  Data
+//
+//  Created by 이윤수 on 6/23/26.
+//  Copyright © 2026 yslee. All rights reserved.
+//
+
+import Foundation
+
+import Domain
+import CoreLocation
+
+public final class LocationRepository: NSObject, LocationRepositoryProtocol {
+    
+    private let locationManager: CLLocationManager
+    private var authContinuation: CheckedContinuation<LocationAuthorizationStatus, Never>?
+    
+    init(locationManager: CLLocationManager = .init()) {
+        self.locationManager = locationManager
+    }
+    
+    public func checkAuthorization() -> LocationAuthorizationStatus {
+        return Self.mapAuthorizationStatus(self.locationManager.authorizationStatus)
+    }
+    
+    public func requestAuthorization() async -> LocationAuthorizationStatus {
+        return await withCheckedContinuation { continuation in
+            self.authContinuation = continuation
+            self.locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    fileprivate static func mapAuthorizationStatus(_ status: CLAuthorizationStatus) -> LocationAuthorizationStatus {
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            return .allowed
+            
+        case .restricted, .denied:
+            return .denied
+            
+        default:
+            return .undetermined
+        }
+    }
+}
+
+extension LocationRepository: CLLocationManagerDelegate {
+    public func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        self.authContinuation?.resume(returning: Self.mapAuthorizationStatus(manager.authorizationStatus))
+        self.authContinuation = nil
+    }
+}
