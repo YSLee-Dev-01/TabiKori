6b25dfeda5e224254cdd8b51b70f7c6ad45cec1c
YSLee-Dev-01 <ysleedev01@gmail.com>
2026-07-07 21:38:59 +0900

    Feat(Main > TouristSpot): 관광지 조회 Endpoint 및 Repository 추가 (CC)

---
+++ Projects/Domain/Sources/RepositoryProtocol/TouristSpotRepositoryProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  TouristSpotRepositoryProtocol.swift
+//  Domain
+//
+//  Created by 이윤수 on 7/7/26.
+//  Copyright © 2026 yslee. All rights reserved.
+//
+
+import Foundation
+
+public protocol TouristSpotRepositoryProtocol: Sendable {
+    func fetchNearbySpots(
+        contentType: CategoryType,
+        coordinate: Coordinate,
+        radiusMeters: Int
+    ) async throws -> [TouristSpot]
+}
