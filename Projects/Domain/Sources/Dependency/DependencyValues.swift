e46ada39d2672fa867981be5aae93ce0c841010c
YSLee-Dev-01 <ysleedev01@gmail.com>
2026-08-17 15:58:34 +0900

    Build: 빌드 오류 개선

--- Projects/Domain/Sources/Dependency/DependencyValues.swift
+++ Projects/Domain/Sources/Dependency/DependencyValues.swift
@@ -20,66 +20,67 @@ extension DependencyValues {
         get {self[LocationUseCaseDependencyKey.self]}
         set {self[LocationUseCaseDependencyKey.self] = newValue}
     }
-
+    
     public var exchangeRateUseCase: ExchangeRateUseCaseProtocol {
         get {self[ExchangeRateUseCaseDependencyKey.self]}
         set {self[ExchangeRateUseCaseDependencyKey.self] = newValue}
     }
-
+    
     public var touristSpotUseCase: TouristSpotUseCaseProtocol {
         get {self[TouristSpotUseCaseDependencyKey.self]}
         set {self[TouristSpotUseCaseDependencyKey.self] = newValue}
     }
-
+    
     public var naverMapUseCase: NaverMapUseCaseProtocol {
         get {self[NaverMapUseCaseDependencyKey.self]}
         set {self[NaverMapUseCaseDependencyKey.self] = newValue}
     }
-
+    
     public var searchHistoryUseCase: SearchHistoryUseCaseProtocol {
         get {self[SearchHistoryUseCaseDependencyKey.self]}
         set {self[SearchHistoryUseCaseDependencyKey.self] = newValue}
     }
-
+    
     public var bookmarkUseCase: BookmarkUseCaseProtocol {
         get {self[BookmarkUseCaseDependencyKey.self]}
         set {self[BookmarkUseCaseDependencyKey.self] = newValue}
     }
-
+    
     public var travelPlanUseCase: TravelPlanUseCaseProtocol {
         get {self[TravelPlanUseCaseDependencyKey.self]}
         set {self[TravelPlanUseCaseDependencyKey.self] = newValue}
     }
-
+    
     public var travelPlanDetailUseCase: TravelPlanDetailUseCaseProtocol {
         get {self[TravelPlanDetailUseCaseDependencyKey.self]}
         set {self[TravelPlanDetailUseCaseDependencyKey.self] = newValue}
     }
-
+    
     public var travelPlanShareUseCase: TravelPlanShareUseCaseProtocol {
         get {self[TravelPlanShareUseCaseDependencyKey.self]}
         set {self[TravelPlanShareUseCaseDependencyKey.self] = newValue}
     }
-
+    
     public var festivalUseCase: FestivalUseCaseProtocol {
         get {self[FestivalUseCaseDependencyKey.self]}
         set {self[FestivalUseCaseDependencyKey.self] = newValue}
     }
-
+    
     public var naverGeocodingUseCase: NaverGeocodingUseCaseProtocol {
         get {self[NaverGeocodingUseCaseDependencyKey.self]}
         set {self[NaverGeocodingUseCaseDependencyKey.self] = newValue}
     }
-
+    
     public var dataResetUseCase: DataResetUseCaseProtocol {
         get {self[DataResetUseCaseDependencyKey.self]}
         set {self[DataResetUseCaseDependencyKey.self] = newValue}
     }
-
+    
     public var subwayStationUseCase: SubwayStationUseCaseProtocol {
         get {self[SubwayStationUseCaseDependencyKey.self]}
         set {self[SubwayStationUseCaseDependencyKey.self] = newValue}
-
+    }
+    
     public var travelItemUseCase: TravelItemUseCaseProtocol {
         get {self[TravelItemUseCaseDependencyKey.self]}
         set {self[TravelItemUseCaseDependencyKey.self] = newValue}
