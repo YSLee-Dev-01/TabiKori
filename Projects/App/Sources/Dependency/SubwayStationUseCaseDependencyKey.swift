64e94367101fbade59a16494d8ec036e2ea59864
YSLee-Dev-01 <ysleedev01@gmail.com>
2026-08-17 15:25:31 +0900

    Feat(App): 지하철역 UseCase 실 구현체 등록 및 시크릿 키 반영 (CC)

---
+++ Projects/App/Sources/Dependency/SubwayStationUseCaseDependencyKey.swift
@@ -0,0 +1,22 @@
+//
+//  SubwayStationUseCaseDependencyKey.swift
+//  App
+//
+//  Created by 이윤수 on 8/17/26.
+//  Copyright © 2026 yslee. All rights reserved.
+//
+
+import Foundation
+
+import ComposableArchitecture
+import Domain
+import Data
+
+extension SubwayStationUseCaseDependencyKey: @retroactive DependencyKey {
+    public static var liveValue: SubwayStationUseCaseProtocol {
+        SubwayStationUseCase(
+            subwayStationRepository: SubwayStationRepository(),
+            naverGeocodingRepository: NaverGeocodingRepository()
+        )
+    }
+}
