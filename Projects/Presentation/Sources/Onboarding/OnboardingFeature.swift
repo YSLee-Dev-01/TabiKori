9319a54cc3a82c626dd42fa093b76c08d3736795
YSLee-Dev-01 <ysleedev01@gmail.com>
2026-09-01 21:21:30 +0900

    Fix(Presentation > Onboarding): 이용약관 체크박스 재선택 시 해제되도록 수정 (CC)

--- Projects/Presentation/Sources/Onboarding/OnboardingFeature.swift
+++ Projects/Presentation/Sources/Onboarding/OnboardingFeature.swift
@@ -100,7 +100,7 @@ public struct OnboardingFeature: Sendable {
 
             case .agreementCheckBoxTapped:
                 guard state.hasViewedPolicy else { return .none }
-                state.isAgreed = true
+                state.isAgreed.toggle()
                 return .none
 
             case .startButtonTapped:
